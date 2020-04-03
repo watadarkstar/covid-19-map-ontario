0450314be8db1234a716154eabc355941e505040
Adrian Carolli <adrian.carolli1@gmail.com>
2020-04-03 12:58:08 -0400

    Fix app

--- src/modules/MainRouter.js
+++ src/modules/MainRouter.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { Router, Route, browserHistory } from 'react-router';
-import App from '../App';
+import GeoTimeline from '../components/GeoTimeline/GeoTimeline';
 import DataView from '../components/DataView';
 
 export default class MainRouter extends Component {
   render() {
     return (
       <Router history={browserHistory}>
-        <Route path="/" component={App} />
+        <Route path="/" component={GeoTimeline} />
         <Route path="/dataview" component={DataView} />
       </Router>
     );
